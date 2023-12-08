4510a07d00048852b1b243487855332cf72cb76f
euxn23 <euxn23@gmail.com>
2023-12-09 00:41:29 +0900

    feat: initial implementation

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,16 +3,19 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'Battle Saga JP',
-  description: 'Battle Saga meetup',
-  icon: '/rspress-icon.png',
-  logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
-  },
+  title: 'Battle Saga',
+  description: 'Battle and Compete Your Favorite Tools.',
+  icon: '/favicon.ico',
+  globalStyles: path.join(__dirname, 'styles/index.scss'),
+  search: false,
   themeConfig: {
+    search: false,
+    hideNavbar: 'always',
+    footer: {
+      message: 'Organized by @euxn23'
+    },
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      { icon: 'github', mode: 'link', content: 'https://github.com/battle-saga-win' },
     ],
   },
 });
